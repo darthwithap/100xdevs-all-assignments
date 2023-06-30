00879bc7721c4865b4324e39fecc77b66a085ca8
patthhar <parthdroidworld@gmail.com>
2023-06-30 16:25:08 +0530

    Some work done on the reconciler - get back at it

--- week-3/01-reconciler/script.js
+++ week-3/01-reconciler/script.js
@@ -2,65 +2,57 @@
 // What are u looking for?
 // You'll get it on Friday you lazy porson
 
+
+
 function createDomElements(data) {
-  var parentElement = document.getElementById("mainArea");
+  var parentEle = document.getElementById('todos_div');
+  var currentChildren = Array.from(parentEle.children);
+  console.log(currentChildren);
 
-  // Get the current children of the parent element and convert it to an array
-  var currentChildren = Array.from(parentElement.children);
+  
+  for (var i=0; i<data.length; i++) {
+    var childEle = document.createElement("div");
+    var titleChild = document.createElement("span");
+    var descriptionChild = document.createElement("span");
+    var deleteButton = document.createElement("button");
 
-  // Process each item in the data array
-  data.forEach(function(item) {
-    // Check if a child with this ID already exists
-    var existingChild = currentChildren.find(function(child) {
-      return child.dataset.id === String(item.id);
-    });
+    titleChild.innerHTML = data[i].title;
+    descriptionChild.innerHTML = data[i].description;
+    
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("onClick", `deleteTodo(${data[i].id})`);
 
-    if (existingChild) {
-      // If it exists, update it
-      existingChild.children[0].innerHTML = item.title;
-      existingChild.children[1].innerHTML = item.description;
-      // Remove it from the currentChildren array
-      currentChildren = currentChildren.filter(function(child) {
-        return child !== existingChild;
-      });
-    } else {
-      // If it doesn't exist, create it
-      var childElement = document.createElement("div");
-      childElement.dataset.id = item.id; // Store the ID on the element for future lookups
+    childEle.appendChild(titleChild);
+    childEle.appendChild(descriptionChild);
+    childEle.appendChild(deleteButton);
 
-      var grandChildElement1 = document.createElement("span");
-      grandChildElement1.innerHTML = item.title
-
-      var grandChildElement2 = document.createElement("span");
-      grandChildElement2.innerHTML = item.description
-
-      var grandChildElement3 = document.createElement("button");
-      grandChildElement3.innerHTML = "Delete"
-      grandChildElement3.setAttribute("onclick", "deleteTodo(" + item.id + ")")
-
-      childElement.appendChild(grandChildElement1)
-      childElement.appendChild(grandChildElement2)
-      childElement.appendChild(grandChildElement3)
-      parentElement.appendChild(childElement);
-    }
-  });
-
-  // Any children left in the currentChildren array no longer exist in the data, so remove them
-  currentChildren.forEach(function(child) {
-    parentElement.removeChild(child);
-  });
+    parentEle.appendChild(childEle);
+  }
 }
 
-//
-// window.setInterval(() => {
-//   let todos = [];
-//   for (let i = 0; i<Math.floor(Math.random() * 100); i++) {
-//     todos.push({
-//       title: "Go to gym",
-//       description: "Go to gym form 5",
-//       id: i+1
-//     })
-//   }
-//
-//   createDomElements(todos)
-// }, 1000)
+// Steps to optimising this
+// Store the existingTodos in array
+// Get the new todos in another array
+// Compare if a particular todo already exists in existingTodos
+// Update the todo with new information just in case
+// Then delete it from the new todos array
+// After you are done scanning with the whole new and old array comparison
+// Make new domElements for the remainder of the data todos in new array
+
+function deleteTodo(id) {
+  console.log(`delete todo called for ${id}`);
+}
+
+
+window.setInterval(() => {
+  let todos = [];
+  for (let i = 0; i<Math.floor(Math.random() * 100); i++) {
+    todos.push({
+      title: "Go to gym",
+      description: "Go to gym at 6",
+      id: i+1
+    })
+  }
+
+  createDomElements(todos)
+}, 20000)
